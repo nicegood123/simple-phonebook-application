92c5cdbd72ff308848929ad713a578d74028e4b7
Gisan Geff Raniego <ggranraniego@gmail.com>
2023-12-14 15:22:16 +0800

    Add and replace passport auth with  sanctum auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
