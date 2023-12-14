8e3a8cac0bb40146d4b0aba0908bca02a66e2e20
Gisan Geff Raniego <ggranraniego@gmail.com>
2023-12-14 11:17:30 +0800

    Install passport auth and lighthouse

--- config/auth.php
+++ config/auth.php
@@ -36,8 +36,13 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
+        // 'web' => [
+        //     'driver' => 'session',
+        //     'provider' => 'users',
+        // ],
+
+        'api' => [
+            'driver' => 'passport',
             'provider' => 'users',
         ],
     ],
